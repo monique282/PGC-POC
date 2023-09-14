16a5f6c76d0be2bdb15a1416e8a32290acd62c53
monique <moniquejp16@gmail.com>
2023-09-14 17:21:05 -0300

    feat: criando a rota de posr de usuario, usersRoutes

---
+++ src/routers/usersRoutes.ts
@@ -0,0 +1,12 @@
+// essa pasta é pra reunir todas as rotas relacionas a usuario
+// ela é chamada la no indexRoutes
+// ela chama a função em controllers
+
+import { Router } from "express";
+
+
+const usersRouter = Router();
+
+usersRouter.post("/users", );
+
+export default usersRouter;
