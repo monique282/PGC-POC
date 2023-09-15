f44e62219b2eb66c179b61cdcdb9383c645a90aa
monique <moniquejp16@gmail.com>
2023-09-15 03:43:23 -0300

    fix: tipando o userSchema, usersSchemas

--- src/schemas/usersSchemas.ts
+++ src/schemas/usersSchemas.ts
@@ -1,6 +1,7 @@
+import { Users } from "@/protocols/protocols";
 import Joi from "joi";
 
-const usersSchema = Joi.object({
+const usersSchema = Joi.object<Users>({
     name: Joi.string().min(2).max(50).required(),
     email: Joi.string().required(),
     password: Joi.string().min(4).required(),
