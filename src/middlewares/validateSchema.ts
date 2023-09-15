a6ed5c5d3a1aa31e3e8dfcdec52a3d149bb33a46
monique <moniquejp16@gmail.com>
2023-09-15 03:44:56 -0300

    fix: tranformando o validadeSchemas para o ts, validadeSchema

---
+++ src/middlewares/validateSchema.ts
@@ -0,0 +1,21 @@
+// essa pasta serve somente pra validade se o que a pessoa digiotou esta correto com o que foi solicitado
+// essa pasta é solicitada em dentro da pasta Routes e usada no arquivo expecifico necessario
+
+import { NextFunction, Request, Response } from "express";
+import { ObjectSchema } from "joi";
+
+export function validateSchemaMiddleware(schema: ObjectSchema) {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const validation = schema.validate(req.body);
+    if (validation.error) {
+      return res.status(422).send({ error: validation.error.message });
+    }
+
+    next();
+  };
+}
+
+
+
+
+
